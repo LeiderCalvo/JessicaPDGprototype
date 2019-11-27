aa9bbfd7a9701aa0882e0d6fb39b6c6298446de2
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2019-11-27 17:58:02 -0500

    lijerisimas interacciones

--- bin/Main.js
+++ bin/Main.js
@@ -14,18 +14,21 @@ new p5(function (app) {
     app.setup = function () {
         app.frameRate(60);
         app.createCanvas(1280, 800);
-            //console.log(preguntas);
+
+        let img0 = app.loadImage("./src/gif1.gif");
+        let img1 = app.loadImage("./src/gif0.gif");
+        let img2 = app.loadImage("./src/seleccionar.png");
 
         for (const prop in preguntas) {
-            slides.push(new Pregunta(app, preguntas[prop], prop));
+            slides.push(new Pregunta(app, preguntas[prop], prop, img0, img1, img2));
         }
         app.smooth();
-
         seleccionada = slides[0];
     }
 
     app.draw = function () {
         app.background(255);
+        app.textFont(app.fontBold);
         seleccionada.pintar();
     }
     
--- bin/Pregunta.js
+++ bin/Pregunta.js
@@ -1,27 +1,23 @@
 class Pregunta {
-    constructor(app, pregunta, id) {
+    constructor(app, pregunta, id, contWhite, contPurple, selec) {
         this.app = app;
         this.pregunta = pregunta;
         this.id = parseInt(id);
         this.respSelc = 0;
+        this.selected = this.pregunta.tipo === "intro" || this.pregunta.tipo === "subintro"? true : false;
 
         this.marginLeft = 50;
         this.marginTop = 80;
         this.img;
+        this.selec = selec;
 
-        if (this.pregunta.tipo === "intro"){
-            this.img = this.app.loadImage("./src/gif1.gif");
-        } else if (this.pregunta.tipo === "subintro"){
-            this.img = this.app.loadImage("./src/gif0.gif");
-        } else {
-            this.img = this.app.loadImage("./src/seleccionar.png");
-        }
+        this.pregunta.tipo === "intro"? this.img = contWhite : this.img = contPurple;
     }
 
     pintar(){
-        this.app.textFont(this.app.fontBold);
         this.app.textSize(40);
         this.app.fill("#8373FF");
+
         let textWidth = 1150;
         if (this.pregunta.tipo === "intro"){
             this.app.background("#8373FF");
@@ -31,10 +27,13 @@ class Pregunta {
             textWidth = 500;
 
         }
+
+
+        
         if(this.app.frameCount % 20 === 0){
-            this.app.image(this.img, 945, 650);
+            this.app.image(this.selected ? this.img : this.selec, 945, 650);
         }else {
-            this.app.image(this.img, 955, 650);
+            this.app.image(this.selected ? this.img : this.selec, 955, 650);
         }
         
         this.app.text(this.pregunta.titulo, this.marginLeft,this.marginTop, textWidth);
@@ -72,6 +71,13 @@ class Pregunta {
             }
 
             this.app.rect(this.marginLeft, dist-40 +(i*95), 55, 55);
+
+            if (this.selected && this.respSelc == i) {
+                this.app.fill('#8373FF');
+                this.app.noStroke();
+                this.app.rect(this.marginLeft, dist-40 +(i*95), 55, 55);
+            }
+
             this.app.fill(0);
             this.app.noStroke();
 
@@ -81,36 +87,40 @@ class Pregunta {
 
     keyboard(){
         if (this.app.keyCode === this.app.UP_ARROW) {
-            if(this.pregunta.tipo.includes("pregunta")){
+            if(this.pregunta.tipo.includes("pregunta") && this.selected === false){
                 if(this.respSelc -1 >= 0) this.respSelc --;
             }
         } else if (this.app.keyCode === this.app.DOWN_ARROW) {
-            if(this.pregunta.tipo.includes("pregunta")){
+            if(this.pregunta.tipo.includes("pregunta") && this.selected === false){
                 if(this.respSelc + 1 < this.pregunta.opciones.length)this.respSelc ++;
             }
         } else if (this.app.keyCode === this.app.RIGHT_ARROW) {
-            let obj = {
-                val: "",
-                titulo: this.pregunta.titulo+"\r\n",
-                respuesta: ""
-            };
 
-            if (this.pregunta.tipo === "pregunta bifurcada") {
-                obj.val = parseInt(this.pregunta.opciones[this.respSelc].direccion);
-                obj.respuesta = this.pregunta.opciones[this.respSelc].respuesta +"\r\n";
-            } else if (this.pregunta.tipo === "pregunta final") {
-                obj.val = 6;
-                obj.respuesta = this.pregunta.opciones[this.respSelc] +"\r\n";
-            } else if (this.id === 6) {
-                obj.val = 0;
-                obj.respuesta = "";
+            if (this.selected) {
+                let obj = {
+                    val: "",
+                    titulo: this.pregunta.titulo+"\r\n",
+                    respuesta: ""
+                };
+    
+                if (this.pregunta.tipo === "pregunta bifurcada") {
+                    obj.val = parseInt(this.pregunta.opciones[this.respSelc].direccion);
+                    obj.respuesta = this.pregunta.opciones[this.respSelc].respuesta +"\r\n";
+                } else if (this.pregunta.tipo === "pregunta final") {
+                    obj.val = 6;
+                    obj.respuesta = this.pregunta.opciones[this.respSelc] +"\r\n";
+                } else if (this.id === 6) {
+                    obj.val = 0;
+                    obj.respuesta = "";
+                }else {
+                    obj.val = this.id + 1;
+                    obj.respuesta = this.pregunta.opciones === undefined? "" : this.pregunta.opciones[this.respSelc] +"\r\n";
+                }
+    
+                return obj;    
             }else {
-                obj.val = this.id + 1;
-                obj.respuesta = this.pregunta.opciones === undefined? "" : this.pregunta.opciones[this.respSelc] +"\r\n";
+                this.selected = true;
             }
-
-            return obj;
-
         }
     }
 }
